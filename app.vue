34fd7e6b0cb79aca334dd52887418f97cbf90fc4
wansoo <jwansoo@hanmail.net>
2023-01-24 20:49:03 +0900

    nuxt_vueschool_2

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+useHead({
+  titleTemplate:(titleChunk)=>{
+    return titleChunk ? `${titleChunk} - My movie site` : 'My movie site'
+  }
+})
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav style="display:flex; gap:10px">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+      <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+      <NuxtLink v-if="!user.isLoggedIn" to="/login">Login</NuxtLink>
+      <a href="#" v-else @click.prevent="user.isLoggedIn=false">Logout</a>
+      test
+    </nav>
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+import { useUser } from '~~/stores/User';
+
+const user=useUser()
+</script>
