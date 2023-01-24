06b060d52f3c4e151db46b3f9a08efd8afbd0d2b
wansoo <jwansoo@hanmail.net>
2023-01-24 22:15:35 +0900

    modify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,10 @@ imports:{
 },
 alias:{
   pinia:'/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs'
+},
+routeRules:{
+  "/spa":{ssr: false},
+  "/static":{static:true},
+  "/swr":{swr:true}
 }
 })
